93c38470160d77657c8755de42f4f7482a2c819c
David Beyda <dbeyda@globo.com>
2017-06-08 00:54:51 -0300

    Database Added 3

--- app.py
+++ app.py
@@ -1,95 +1,141 @@
 from flask import Flask, jsonify, abort, make_response, request, url_for
+from datetime import datetime
 
 app = Flask(__name__)
 
-notas = [
+# Dados para testes iniciais
+invoices = [
 {
 	'id': 1,
-	'cod': 285,
-	'title': 'Farmacia da Republica',
-	'valor total': 20.0
+	'ReferenceMonth': 1,
+	'ReferenceYear': 2016,
+    'Document': 'Outback',
+    'Description': 'Batata, Cebola, 5 Refil',
+    'Amount': 99.35,
+    'IsActive': 1,
+    'CreatedAt': datetime(2016, 1, 3, hour = 12, minute = 54, second = 3),
+    'DeactiveAt': None
 },
 {
 	'id': 2,
-	'cod': 937,
-	'title': 'Outback Steakhouse',
-	'valor total': 42.99
+	'ReferenceMonth': 4,
+	'ReferenceYear': 2016,
+    'Document': 'Bodytech Academia',
+    'Description': 'Plano Silver 6 meses',
+    'Amount': 350*6,
+    'IsActive': 1,
+    'CreatedAt': datetime(2016, 4, 25, hour = 17, minute = 37, second = 48),
+    'DeactiveAt': None
+},
+{
+	'id': 3,
+	'ReferenceMonth': 5,
+	'ReferenceYear': 2016,
+    'Document': 'Big Bi',
+    'Description': 'Coco Gelado',
+    'Amount': 6.5,
+    'IsActive': 0,
+    'CreatedAt': datetime(2016, 5, 12, hour = 10, minute = 3, second = 4),
+    'DeactiveAt': datetime(2016, 5, 19, hour = 16)
 }]
 
+field_list = ['ReferenceMonth', 'ReferenceYear', 'Document', 'Description', 'Amount', 'IsActive', 'CreatedAt', 'DeactiveAt']
 
 
 #GET ALL
-@app.route('/nf/api/v1.0/notas', methods=['GET'])
-def get_notas():
-    return jsonify({'notas': [nota_publica(nota) for nota in notas]})
+@app.route('/nf/api/v1.0/invoices', methods=['GET'])
+def get_invoices():
+    return jsonify({'invoices': [invoice_publica(invoice) for invoice in invoices]})
 
 
 #GET ID
-@app.route('/nf/api/v1.0/notas/<int:nota_id>', methods=['GET'])
-def get_nota(nota_id):
-	nota = [anota for anota in notas if anota['id'] == nota_id]
-	if len(nota) == 0:
+@app.route('/nf/api/v1.0/invoices/<int:invoice_id>', methods=['GET'])
+def get_invoice(invoice_id):
+	invoice = [invoice for invoice in invoices if invoice['id'] == invoice_id]
+	if len(invoice) == 0:
 		abort(404)
-	return jsonify({'nota': nota_publica(nota[0])})
+	return jsonify({'invoice': invoice_publica(invoice[0])})
 
 
 #POST
-@app.route('/nf/api/v1.0/notas', methods = ['POST'])
-def create_nota():
-	if not request.json or not 'title' in request.json or not 'cod' in request.json or not 'valor total' in request.json:
-
-
+@app.route('/nf/api/v1.0/invoices', methods = ['POST'])
+def create_invoice():
+	# if not request.json or not 'ReferenceMonth' in request.json or not 'cod' in request.json or not 'valor total' in request.json:
+	# 	abort(400)
+	for field in field_list:
+		if not field in request.json:
+			abort(400)
+	if not request.json:
 		abort(400)
-	nota = {
-	'id': notas[-1]['id'] + 1, #id da ultima nota +1
-	'cod': request.json['cod'],
-	'title': request.json['title'],
-	'valor total': request.json['valor total']
+	invoice = {
+	'id': invoices[-1]['id'] + 1, #id da ultima invoice +1
+	'ReferenceMonth': request.json['ReferenceMonth'],
+	'ReferenceYear': request.json['ReferenceYear'],
+	'Document': request.json['Document'],
+	'Description': request.json['Description'],
+	'Amount': request.json['Amount'],
+	'IsActive': request.json['IsActive'],
+	'CreatedAt': request.json['CreatedAt'],
+	'DeactiveAt': request.json['DeactiveAt']
 	}
-	notas.append(nota)
-	return jsonify({'nota': nota_publica(nota)}), 201
+	invoices.append(invoice)
+	return jsonify({'invoice': invoice_publica(invoice)}), 201
 
 
 #UPDATE
-@app.route('/nf/api/v1.0/notas/<int:nota_id>', methods = ['PUT'])
-def update_nota(nota_id):
-	nota = [nota for nota in notas if nota['id'] == nota_id]
+@app.route('/nf/api/v1.0/invoices/<int:invoice_id>', methods = ['PUT'])
+def update_invoice(invoice_id):
+	invoice = [invoice for invoice in invoices if invoice['id'] == invoice_id]
 	
 	# Restricoes e conferencia das informacoes
-	if len(nota) == 0: abort(404)
-	if not request.json: abort (404)
-	if 'cod' in request.json and type(request.json['cod']) != int: abort(404)
-	if 'title' in request.json and type(request.json['title']) != str: abort(404)
-	if 'valor total' in request.json and type(request.json['valor total']) != float: abort(404)
+	if len(invoice) == 0:
+		abort(404)
+	if not request.json:
+		abort (404)
+	if 'ReferenceMonth' in request.json and type(request.json['ReferenceMonth']) != int:
+		abort(404)
+	if 'ReferenceYear' in request.json and type(request.json['ReferenceYear']) != int:
+		abort(404)
+	if 'Description' in request.json and (type(request.json['Description']) != str or len(request.json['Description']) > 256):
+		abort(404)
+	if 'Document' in request.json and (type(request.json['Document']) != str or len(request.json['Document']) > 14):
+		abort(404)
+	if 'Amount' in request.json and type(request.json['Amount']) != float:
+		abort(404)
+	if 'IsActive' in request.json and (type(request.json['IsActive']) != int or request.json['IsActive'] < 0 or request.json['IsActive'] > 256):
+		abort(404)
+	# if 'CreatedAt' in request.json and type(request.json['CreatedAt']) != float:
+	# 	abort(404)
+	# if 'DeactiveAt' in request.json and type(request.json['DeactiveAt']) != float:
+	# 	abort(404)
 
-	nota[0] ['cod'] = request.json['cod']
-	nota[0]['title'] = request.json['title']
-	nota[0]['valor total'] = request.json['valor total']
-	return jsonify({"nota": nota_publica(nota[0])})
+	for campo in field_list:
+		invoice[0][campo] = request.json[campo]
+	return jsonify({"invoice": invoice_publica(invoice[0])})
 
 #DELETE
-@app.route('/nf/api/v1.0/notas/<int:nota_id>', methods = ['DELETE'])
-def delete_nota(nota_id):
-	nota = [nota for nota in notas if nota['id'] == nota_id]
-	if len(nota) == 0:
+@app.route('/nf/api/v1.0/invoices/<int:invoice_id>', methods = ['DELETE'])
+def delete_invoice(invoice_id):
+	invoice = [invoice for invoice in invoices if invoice['id'] == invoice_id]
+	if len(invoice) == 0:
 		abort(404)
-	notas.remove(nota[0])
+	invoices.remove(invoice[0])
 	return jsonify({"result": True})
 
-#Retornar URI, além do ID
-def nota_publica(nota):
-	nova_nota = {}
-	for campo in nota:
+#Retornar URI, no lugar do ID
+def invoice_publica(invoice):
+	nova_invoice = {}
+	for campo in invoice:
 		if campo == 'id':
-			nova_nota['uri'] = url_for('get_nota', nota_id = nota['id'], _external = True)
+			nova_invoice['uri'] = url_for('get_invoice', invoice_id = invoice['id'], _external = True)
 		else:
-			nova_nota[campo] = nota[campo]
-	return nova_nota
+			nova_invoice[campo] = invoice[campo]
+	return nova_invoice
 
 
 
-
-@app.errorhandler(404)  # para retornar Json em vez de HTTP
+# quando ocorrer erro 404 retornar Json em vez de HTTP
+@app.errorhandler(404)
 def not_found(error):
 	return make_response(jsonify({'error': 'Not found'}), 404)
 
